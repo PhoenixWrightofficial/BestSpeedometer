ecf8bb8f8282c00185b6157c29038a2089058e0d
PhoenixWrightofficial <xxmidasazer@gmail.com>
2026-08-01 00:28:08 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <mod/amlmod.h>
+#include <mod/logger.h>
+
+MYMOD(net.phoenixwright.best_speedometer, BestSpeedometer, 1.0)
+
+extern "C" void OnModLoad()
+{
+    logger->SetTag("BestSpeedometer");
+    logger->Info("BestSpeedometer loaded!");
+}
